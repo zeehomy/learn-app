dfffbaf1ce90cb24ce2ec21c2834f0eb51c9edb6
yuezihao <yuezihao@joinquant.com>
2019-03-26 13:42:07 +0800

    update list tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FlipMove from 'react-flip-move';
+import Tabs from './Tabs';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: '1', name: 'Cras justo odi' },
+        { id: '2', name: 'Dapibus ac facilisis in' },
+        { id: '3', name: 'Morbi leo risus' },
+        { id: '4', name: 'Porta ac consectetur ac' },
+        { id: '5', name: 'Vestibulum at eros' },
+      ]
+    };
+    
+  }
+  
   render() {
+    let items = this.state.items;
     return (
-      <div className="app">
-        <h1>hello1</h1>
+      <div className="items-tabs-panel">
+        <ul>
+          <FlipMove>
+            {
+              items.map((item, index) => (
+                <li key={item.id} className="list-group-item">
+                  <div className="name">{item.name}</div>
+                  <div className="btn-group">
+                    {
+                      index > 0 ? 
+                        <button className="btn btn-primary" onClick={() => this.move(index, -1)}>Up</button> : null
+                    }
+                    {
+                      index < items.length - 1 ? 
+                        <button className="btn btn-dark" onClick={() => this.move(index, 1)}>Down</button> : null
+                    }
+                  </div>
+                </li>
+              ))
+            }
+          </FlipMove>
+        </ul>
+        <Tabs></Tabs>
       </div>
-    );
+    )
+  }
+  
+  move(index, diff) {
+    let items = this.state.items;
+    let item = items[index];
+    items.splice(index, 1);
+    items.splice(index + diff, 0, item);
+    this.setState({ items: items });
   }
 }
 
---
+++ src/Tabs.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class Tabs extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tabs: [
+				{ id: 1, name: 'Item A', isActive: true},
+				{ id: 2, name: 'Item B', isActive: false},
+				{ id: 3, name: 'Item C', isActive: false}
+			]
+		};
+	}
+
+	render() {
+		let tabs = this.state.tabs;
+		return (
+			<div className="tabs-panel">
+				<div className="buttons">
+					{
+						tabs.map((item, index) => (
+							<div key={item.id} className={item.isActive && 'active'} onClick={() => this.clickTab(index)}>
+								{item.name}
+							</div>
+						))
+					}
+				</div>
+				<span>
+					{
+						tabs.find((item) => item.isActive).name
+					}
+				</span>
+			</div>
+		)
+	}
+
+	clickTab(index) {
+		let tads = this.state.tabs;
+		tads.map((item) => item.isActive = false);
+		tads[index].isActive = true;
+		this.setState({
+			tabs: tads
+		});
+	}
+}
+
+export default Tabs;
